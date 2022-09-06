dbf29640a145924bdbdcb6d8f76201e20fc7931e
Derek Colley <derek@colley.cc>
2022-09-06 11:45:07 +0100

    add urlhandler

--- app/app.ts
+++ app/app.ts
@@ -1,11 +1,18 @@
 import Vue from 'nativescript-vue'
 import Home from './components/Home.vue'
 
+import { handleOpenURL, AppURL } from 'nativescript-urlhandler';
+ 
 declare let __DEV__: boolean;
 
 // Prints Vue logs when --env.production is *NOT* set while building
 Vue.config.silent = !__DEV__
 
 new Vue({
+  created() {
+    handleOpenURL((appURL: AppURL) => {
+      console.log('Got the following appURL', appURL);
+    });
+  },
   render: (h) => h('frame', [h(Home)]),
 }).$start()
